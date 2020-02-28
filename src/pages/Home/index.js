22e6762c6ad741a9b3d0ceb81b3e5eb08c1adb22
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-02-28 17:31:47 -0300

    Estilizando a página Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,147 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles'
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <ProductList>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
+            alt="Tênis"
+          />
+          <strong>Nike Nightgazer</strong>
+          <span>R$ 229,99</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 1
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      </ProductList>
     </>
   );
 }
