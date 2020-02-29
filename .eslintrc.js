493c008d206775641469f4cc33b4d128ddcd9b5a
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-02-28 23:22:08 -0300

    Usando immer para verificar produtos duplicados.

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'react/static-property-placement': 'off',
-    'no-console': ["error", { allow: ["tron"]}]
+    'no-console': ["error", { allow: ["tron"]}],
+    'no-param-reassign': 'off'
   },
 };
