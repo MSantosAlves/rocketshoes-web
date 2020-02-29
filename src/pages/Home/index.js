c95c0498cd95a9e6fd93c72745e06f90b630cb3c
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-02-28 22:13:56 -0300

    Buscando dados da api e formatando preço.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,147 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../utils/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <>
-      <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+    this.setState({ products: data });
+  }
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-nightgazer-masculino/10/D12-3139-010/D12-3139-010_zoom1.jpg?ims=544x"
-            alt="Tênis"
-          />
-          <strong>Nike Nightgazer</strong>
-          <span>R$ 229,99</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 1
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      </ProductList>
-    </>
-  );
+  render() {
+    const { products } = this.state;
+    return (
+      <>
+        <ProductList>
+          {products.map(product => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+              <button type="button">
+                <div>
+                  <MdAddShoppingCart size={16} color="#FFF" /> 1
+                </div>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))}
+        </ProductList>
+      </>
+    );
+  }
 }
